84c39646175940d6d84254e117862078610d6667
jueun275 <52851721+jueun275@users.noreply.github.com>
2022-12-23 00:09:36 +0900

    Create LoginDto.java

---
+++ src/main/java/com/jun/springbootjwt/endpoint/dto/LoginDto.java
@@ -0,0 +1,24 @@
+package com.jun.springbootjwt.endpoint.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@NoArgsConstructor
+public class LoginDto {
+    @NotNull
+    private String email;
+    @NotNull
+    private String password;
+
+    private LoginDto(final String email, final String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public static LoginDto of(final String email, final String password) {
+        return new LoginDto(email, password);
+    }
+}
